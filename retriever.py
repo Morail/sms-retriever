a6498102095fd482bae4cf92e0ad515b910de3d4
Marco Frassoni <marco.frassoni@gmail.com>
2010-11-25 15:14:55 +0100

    saving messages in a dict

--- retriever.py
+++ retriever.py
@@ -14,44 +14,104 @@
 ##########################################################################
 
 
+import sys
 import urllib2 as ul
-from BeautifulSoup import BeautifulSoup
+import logging
 import re
-from datetime import datetime
+import htmlentitydefs
 
+from datetime import datetime
+from BeautifulSoup import BeautifulSoup
+
+
+class Retriever(object):
+    
+    def __init__(self, url, mpp=1, df='%y/%m/%d', regex='.'):
+        self.top = mpp
+        self.date_format = df
+        self.counter, self.pagestart = 0, 0
+        self.base_url = url
+        self.messages = {}
+        self.regex = regex
+
+
+    def process(self):
+    
+        while True:
+            url = "%s?top=%d&pagestart=%d" % (self.base_url, 
+                                              self.top, self.pagestart)
+            
+            html = BeautifulSoup(ul.urlopen(url))
+    
+            ## break while loop
+            if not html:
+                logging.debug('Nothing returned from the given url, stopping this process')
+                break
+    
+            list_ = html.findAll('div', id=re.compile(self.regex))
+    
+            if not len(list_):
+                logging.debug('No more messages found, stopping this process')
+                break
+    
+            for d in list_:
+                id = d['id'].split('_')[-1]
+
+                if not id or id in messages:
+                    continue
+
+                date_ = datetime.strptime(d.strong.string, self.date_format)
+                text= unescape(d.div.contents[-1])
+    
+                self.messages[id] = (text, date_)
+
+                ## updating message counter
+                self.counter += 1
+    
+            ## incrementing pagestart
+            self.pagestart += self.top
+    
+        logging.info('Messages: %d' % (self.counter,))
+
+
+## Thanks to: http://effbot.org/zone/re-sub.htm#unescape-html
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
 
 def main():
-    base_url = 'http://www.rtl.it/ajaxreturn/sms_list.php'
-    top = 15
-    pagestart = 0
-    date_format = "%d.%m.%Y %H:%M:%S"
-    counter = 0
 
-    while True:
-        url = "%s?top=%d&pagestart=%d" % (base_url, top, pagestart)
-        html = BeautifulSoup(ul.urlopen(url))
+    logging.basicConfig(#filename="sms-retriever.log",
+                                stream=sys.stderr,
+                                level=logging.DEBUG)
 
-        if not html: break
+    logging.info('START')
+    
+    url = 'http://www.rtl.it/ajaxreturn/sms_list.php'
+    df = '%d.%m.%Y %H:%M:%S'
+    regex = 'sms_\d+'
+    
+    r = Retriever(url=url, mpp=15, regex=regex, df=df)
+    r.process()
 
-        #list_ = html.findAll('div', id=lambda x: x and re.findall('sms_\d+', x))
-        list_ = html.findAll('div', id=re.compile('sms_\d+'))
-
-        if not len(list_): break
-
-        for d in list_:
-            id = d['id'].split('_')[-1]
-            date_ = datetime.strptime(d.strong.string, date_format)
-            text= ul.unquote(d.div.contents[-1])
-
-            print id, date_, text
-
-            counter += 1
-
-        ## incrementing pagestart
-        pagestart += top
-
-    print 'Messages:', counter
-
+    logging.info('STOP')
 
 if __name__ == "__main__":
     main()
